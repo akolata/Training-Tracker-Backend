a2c95d2a8d2d897b8f18cfdf092e830e9171beae
Aleksander Kolata <olek.tbg.konta@gmail.com>
2019-09-14 22:57:29 +0200

    Basic project structure created

---
+++ src/main/java/pl/akolata/trainingtracker/TrainingTrackerApplication.java
@@ -0,0 +1,13 @@
+package pl.akolata.trainingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingTrackerApplication.class, args);
+    }
+
+}
